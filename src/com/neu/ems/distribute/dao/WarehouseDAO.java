9876c6df0751b499c4b6e000f8deff1843bbacaa
kobeyy <immortal_empire@outlook.com>
2018-06-19 15:42:49 +0800

    ddddddddddddddddddd

--- src/com/neu/ems/distribute/dao/WarehouseDAO.java
+++ src/com/neu/ems/distribute/dao/WarehouseDAO.java
@@ -1,5 +1,5 @@
 package com.neu.ems.distribute.dao;
 
 public interface WarehouseDAO {
-
+	void test();
 }
--- src/com/neu/ems/distribute/dao/WarehouseDAOImp.java
+++ src/com/neu/ems/distribute/dao/WarehouseDAOImp.java
@@ -2,4 +2,10 @@ package com.neu.ems.distribute.dao;
 
 public class WarehouseDAOImp implements WarehouseDAO {
 
+	@Override
+	public void test() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
